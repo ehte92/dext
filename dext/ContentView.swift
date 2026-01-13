5ca65bfaa65c3c51de974117dcadb471188343b6
Ehtesham Ansari <46630915+ehte92@users.noreply.github.com>
2026-01-13 23:50:26 +0300

    Initial Commit

---
+++ dext/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dext
+//
+//  Created by Ehtesham Ansari on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dext/dextApp.swift
@@ -0,0 +1,17 @@
+//
+//  dextApp.swift
+//  dext
+//
+//  Created by Ehtesham Ansari on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct dextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
